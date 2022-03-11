3655119199ccc8f767271417a8a66093c29d2468
snowmerak <snowmerak@outlook.com>
2022-03-12 00:11:07 +0900

    update test code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var docsPath = filepath.Join(".", "docs")
@@ -15,12 +16,47 @@ func init() {
 }
 
 func main() {
+	createMainPage()
+}
+
+func createMainPage() {
 	f, err := os.Create(filepath.Join(docsPath, "index.html"))
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	f.WriteString(fmt.Sprintf(header, "Hello, World!"))
-	f.WriteString(fmt.Sprintf(body, "<h1>Hello, World!</h1>"))
+	f.WriteString(fmt.Sprintf(header, "Index Page"))
+
+	sb := new(strings.Builder)
+	sb.WriteString("<table>")
+
+	titles := []string{"name", "score"}
+	sb.WriteString("<thead><tr>")
+	for _, title := range titles {
+		sb.WriteString("<th>")
+		sb.WriteString(title)
+		sb.WriteString("</th>")
+	}
+	sb.WriteString("</tr></thead>")
+
+	userDir := filepath.Join(".", "users")
+	dirs, err := os.ReadDir(userDir)
+	if err != nil {
+		panic(err)
+	}
+	sb.WriteString("<tbody>")
+	for _, dir := range dirs {
+		if dir.IsDir() {
+			sb.WriteString("<tr>")
+			sb.WriteString("<td>")
+			sb.WriteString(dir.Name())
+			sb.WriteString("</td>")
+			sb.WriteString("</tr>")
+		}
+	}
+	sb.WriteString("</tbody>")
+	sb.WriteString("</table>")
+
+	f.WriteString(fmt.Sprintf(body, sb.String()))
 }
