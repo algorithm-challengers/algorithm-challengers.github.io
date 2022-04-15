22e7c2b269d40d0636fd9d8fcaf45c5d1b3cd44c
snowmerak <snowmerak@outlook.com>
2022-04-15 20:22:16 +0900

    bundle up

--- src/users.js
+++ src/users.js
@@ -1,20 +1,38 @@
+import { solveds } from "./solved";
 
 export class User {
-    constructor(name, contact, easy, medium, hard) {
+    constructor(name, contact) {
         this.name = name;
         this.contact = contact;
-        this.easy = easy;
-        this.medium = medium;
-        this.hard = hard;
-
-        this.score = easy + medium + hard;
+        this.easy = 0;
+        this.medium = 0;
+        this.hard = 0;
+        this.score = 0;
     }
 }
 
-export const users = [
-    new User('냥냥이', '냥냥이@삐약삐약.com', 2, 0, 0),
-    new User('snowmerak', '@snowmerak', 2, 0, 0),
-    new User('lemon-mint', '', 1, 0, 0),
+export let users = [
+    new User('냥냥이', '냥냥이@삐약삐약.com'),
+    new User('snowmerak', '@snowmerak'),
+    new User('lemon-mint', '@lemon-mint'),
 ]
 
 users.sort((a, b) => b.score - a.score);
+
+for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    let solved = solveds[user.name];
+    if (solved) {
+        for (let j = 0; j < solved.length; j++) {
+            let [date, level] = solved[j].split(':');
+            if (level === 'easy') {
+                user.easy++;
+            } else if (level === 'medium') {
+                user.medium++;
+            } else if (level === 'hard') {
+                user.hard++;
+            }
+        }
+        user.score = user.easy + user.medium + user.hard;
+    }
+}
