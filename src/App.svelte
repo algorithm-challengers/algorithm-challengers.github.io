ebd2d5b9511c513e339d913aaaf941f9f2dbaff2
snowmerak <snowmerak@outlook.com>
2022-04-19 19:33:24 +0900

    add 날코냥이 04-19 solved and 소개

--- src/App.svelte
+++ src/App.svelte
@@ -14,12 +14,24 @@
 
 <main>
 	<h1>Algorithm Challengers</h1>
-	<button type="button" on:click={setTap(0)}>똑냥이 랭킹</button>
-	<button type="button" on:click={setTap(1)}>문제</button>
-	<button type="button" on:click={setTap(2)}>냥방</button>
+	<button type="button" on:click={setTap(0)}>소개</button>
+	<button type="button" on:click={setTap(1)}>똑냥이 랭킹</button>
+	<button type="button" on:click={setTap(2)}>문제</button>
+	<button type="button" on:click={setTap(3)}>냥방</button>
 	<hr />
 
 	{#if tap == 0}
+		<h3>2022 코딩 테스트 성실 냥이 콘테스트</h3>
+		<p>1. 일주일에 3~5일 정도 가량 냥냥이가 엄선한 문제가 올라오는 거시애옹</p>
+		<p>2. 청정수 문제 1개, 썩은물 문제 1개씩 내는 거시애옹</p>
+		<p>3. 코딩테스트를 완료 하였다면 문제 웹 페이지의 전체를 캡처해서 냥방에 올리면 되는 거시야옹</p>
+		<p>4. 문제를 풀 때마다 성실 냥이 점수가 1점씩 올르는 거시애옹</p>
+		<p>5. 몇일 지난 문제도 풀어도 점수가 인정되는 거시애옹</p>
+		<p>6. 문제 목록은 이 공지글의 댓글에 달리는 거시애옹</p>
+		<p>7. 각종 개선사항 및 문의는 눈냥이한테 물어봐달라냥</p>
+	{/if}
+
+	{#if tap == 1}
 		{#each users as user, i}
 			<h3>{i+1} : {user.name} ({user.contact})</h3>
 			<details>
@@ -45,7 +57,7 @@
 		{/each}
 	{/if}
 
-	{#if tap == 1}
+	{#if tap == 2}
 		{#each problems as problem}
 			<h3>{problem.date}</h3>
 			<details>
@@ -63,7 +75,7 @@
 		{/each}
 	{/if}
 
-	{#if tap == 2}
+	{#if tap == 3}
 		<p>[대충 냥냥이가 쓸 소개말]</p>
 		<a href="https://open.kakao.com/o/gbCkR4zd">카카오톡 오픈톡 링크</a>
 	{/if}
