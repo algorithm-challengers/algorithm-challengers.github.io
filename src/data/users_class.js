69f87939fb9d77e8601f557ca91b88e9852bf950
snowmerak <snowmerak@outlook.com>
2022-04-20 17:19:05 +0900

    merge users and solveds

---
+++ src/data/users_class.js
@@ -0,0 +1,23 @@
+export class User {
+    constructor(name, contact, solveds) {
+        this.name = name;
+        this.contact = contact;
+        this.solveds = solveds;
+        this.easy = 0;
+        this.medium = 0;
+        this.hard = 0;
+
+        for (let i = 0; i < solveds.length; i++) {
+            let [date, level] = solveds[i].split(':');
+            if (level === 'easy') {
+                this.easy++;
+            } else if (level === 'medium') {
+                this.medium++;
+            } else if (level === 'hard') {
+                this.hard++;
+            }
+        }
+
+        this.score = this.easy * 10 + this.medium * 20 + this.hard * 30;
+    }
+}
