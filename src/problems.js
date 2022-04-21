6df5bf1da5387b3239cd99babc494eda2a667df3
narcotis <qhguswkd@gmail.com>
2022-04-21 19:13:33 +0900

    Update problems.js

--- src/problems.js
+++ src/problems.js
@@ -14,5 +14,6 @@ export const problems = [
     new Problem('2022-04-18', 'https://programmers.co.kr/learn/courses/30/lessons/81301', '', ''),
     new Problem('2022-04-15', 'https://programmers.co.kr/learn/courses/30/lessons/42748', '', ''),
     new Problem('2022-04-14', 'https://programmers.co.kr/learn/courses/30/lessons/42840', '', ''),
-    new Problem('2020-04-13', 'https://programmers.co.kr/learn/courses/30/lessons/77884', '', ''),
+    new Problem('2022-04-13', 'https://programmers.co.kr/learn/courses/30/lessons/77884', '', ''),
+    new Problem('2022-04-21', 'https://programmers.co.kr/learn/courses/30/lessons/1845', '',''),
 ]
