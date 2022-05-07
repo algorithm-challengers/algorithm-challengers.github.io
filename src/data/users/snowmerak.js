d911eef125532b96d292fcfc4df1bdc3060090df
snowmerak <snowmerak@outlook.com>
2022-05-07 12:05:46 +0900

    Update snowmerak.js

--- src/data/users/snowmerak.js
+++ src/data/users/snowmerak.js
@@ -7,6 +7,10 @@ let user = new User('snowmerak', 'snowmerak', [
     '2022-04-18:easy',
     '2022-04-19:easy',
     '2022-04-20:easy',
+    '2022-04-21:easy',
+    '2022-04-23:easy',
+    '2022-04-26:easy',
+    '2022-04-27:easy',
 ]);
 
 users.push(user);
