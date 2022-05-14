dc6453f33a7529d4eb62fe362e6c6c5881d5fa9d
snowmerak <snowmerak@outlook.com>
2022-05-14 18:45:49 +0900

    Update 날코.js

--- src/data/users/날코.js
+++ src/data/users/날코.js
@@ -13,9 +13,9 @@ let user = new User('날코', 'narcotis', [
     '2022-04-26:easy',
     '2022-04-27:easy',
     '2022-05-01:medium',
-    '2022-05-05:medium'
-    '2022-05-06:medium'
-    '2022-05-07:medium'
+    '2022-05-05:medium',
+    '2022-05-06:medium',
+    '2022-05-07:medium',
 ]);
 
 users.push(user);
